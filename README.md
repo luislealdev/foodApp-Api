4194ca87543e3fb80b00bcc7a29737fe53e90861
Luis Leal <luisrrleal@gmail.com>
2024-01-06 16:44:56 -0600

    Add file sistem

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 ## How to run the API
 
-1. Clone this repo and install all packages
-2. Run ``
+1. Clone this repo and install all packages with `yarn` (or your favorite package manager)
+2. Run `docker-compose up -d` to download the image and create the container.
+3. After the container is running, run `prisma migrate dev` to create the db and the models.
+
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import express, { Request, Response } from 'express';
+const prisma = require('./db/prisma');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, TypeScript Express!');
+});
+
+app.get('/seed', (req: Request, res: Response) => {
+    try {
+
+    } catch (error) {
+
+    }
 });
 
 
